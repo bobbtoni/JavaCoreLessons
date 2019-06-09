baaa27d9035aa5f3d867a3226632aaa0d4b32e13
Anton <bobb.toni.phone@gmail.com>
2019-06-09 22:12:27 +0300

    Исзодный код второго урока

---
+++ src/ru/geekbrains/lesson2Re/Outer.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.lesson2Re;
+
+public class Outer {
+
+    private int outerVar;
+
+    class Inner {
+        private int innerVar;
+
+        public Inner(int innerVar) {
+            this.innerVar = innerVar;
+        }
+
+        void innerTest() {
+            System.out.println(innerVar);
+            System.out.println(outerVar);
+        }
+    }
+
+    public Outer(int outerVar) {
+        this.outerVar = outerVar;
+    }
+
+    void outerTest() {
+      //  System.out.println(innerVar);
+        System.out.println(outerVar);
+    }
+}
+
+
+class MainOuterInner {
+    public static void main(String[] args) {
+       // Outer outer = new Outer(10);
+        Outer.Inner inner = new Outer(10).new Inner(50);
+    }
+}
